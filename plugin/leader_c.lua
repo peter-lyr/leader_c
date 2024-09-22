086c519e3a60976bd6cf6debcac48e105811896d
peter-lyr <llydrp.ldp@gmail.com>
2024-09-22 13:04:24 +0800

    leader_c

--- plugin/leader_c.lua
+++ plugin/leader_c.lua
@@ -1,4 +1,5 @@
 require 'which-key'.register {
   ['<leader>c'] = { name = 'comment', },
-  ['<leader>cp'] = { function()  end, 'comment: paragraph', mode = { 'n', }, },
+  ['<leader>cp'] = { "}kvip:call nerdcommenter#Comment('x', 'toggle')<CR>", 'comment: paragraph', mode = { 'n', }, },
+  ['<leader>co'] = { "}kvip:call nerdcommenter#Comment('x', 'invert')<CR>", 'comment: paragraph', mode = { 'n', }, },
 }
