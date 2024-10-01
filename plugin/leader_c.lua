4fd59f8710948552c0db3d8a945da5eed9e65ce9
peter-lyr <llydrp.ldp@gmail.com>
2024-10-01 19:53:43 +0800

    <leader>ck F.cmake()

--- plugin/leader_c.lua
+++ plugin/leader_c.lua
@@ -1,5 +1,8 @@
+local F = require 'f'
+
 require 'which-key'.register {
   ['<leader>c'] = { name = 'comment', },
   ['<leader>cp'] = { "}kvip:call nerdcommenter#Comment('x', 'toggle')<CR>", 'comment: paragraph', mode = { 'n', }, },
   ['<leader>co'] = { "}kvip:call nerdcommenter#Comment('x', 'invert')<CR>", 'comment: paragraph', mode = { 'n', }, },
+  ['<leader>ck'] = { function() F.cmake() end, 'cmake', mode = { 'n', }, },
 }
